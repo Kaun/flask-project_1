0713af477feab868b82fcdcf9da566e0f0528321
Kaun <kaunnikov@gmail.com>
2020-03-27 09:56:45 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def route_index():
+    return render_template("index.html")
+
+
+@app.route('/depature/<depature>')
+def route_depature(depature):
+    return render_template("depature.html", depature=depature)
+
+
+@app.route('/tour/<id>')
+def route_tour(id):
+    return render_template("tour.html", id=id)
+
+@app.route('/test/<role>')
+def route_test(role):
+    return render_template("test.html", role=role)
+
+
+if __name__ == '__main__':
+    app.run()
